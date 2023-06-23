b6fea0161c25afa2c577808a313e03b41332710c
EsteMaeLeo <leonel_morales20@hotmail.com>
2023-06-22 23:53:22 -0500

    initial commit

---
+++ src/main/java/hello/world/com/HelloWoldSpring/HelloWoldSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.world.com.HelloWoldSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWoldSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWoldSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/world/com/HelloWoldSpring/HelloWoldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.world.com.HelloWoldSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWoldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
